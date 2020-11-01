4f77dbfba200fe0185f7738b2402fa93ab34889e
Adrianna Gilmore <a_inman33@yahoo.com>
2020-11-01 00:58:16 -0700

    17 tests passed

--- queue.py
+++ queue.py
@@ -1,11 +1,24 @@
 # Queue: A queue.
 # Your implementation should pass the tests in test_queue.py.
-# YOUR NAME
+# Adrianna Gilmore
 
 # Hint: pip3 install llist
-# from llist import sllist
+from llist import sllist
 
 class Queue:
 
-    pass
+    def __init__(self):
+        self.data = sllist()
+        self.length = 0
+
+    def enqueue(self, value):
+        self.data.append(value)
+        self.length += 1
+
+    def dequeue(self):
+        self.length -= 1
+        return self.data.popleft()
+    
+    def is_empty(self):
+        return self.length == 0
 
